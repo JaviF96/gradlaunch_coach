f8704b72e14e5443281bf407073f0945a4ccf8e7
Javi <javier.fimia@gmail.com>
2026-09-05 11:05:44 +0100

    Changed logo

--- frontend/src/components/ChromeBar.tsx
+++ frontend/src/components/ChromeBar.tsx
@@ -1,16 +1,10 @@
+import logo from "../assets/gradlaunch-logo.png";
+
 export function ChromeBar() {
   return (
     <header className="gl-chrome">
       <div className="gl-brand">
-        <svg
-          className="gl-mark"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
-          focusable="false"
-        >
-          <path d="M2 21l21-9L2 3v7l15 2-15 2z" />
-        </svg>
-        <span className="gl-wordmark">GradLaunch</span>
+        <img className="gl-logo" src={logo} alt="GradLaunch" />
       </div>
       <span className="gl-chrome-rule" aria-hidden="true" />
       <h1 className="gl-product-name">Application feedback</h1>
